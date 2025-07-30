9fc2964f23d629960bda6ed182fe8cb000326606
myamaguchi <tedako.jp@coderdojo.com>
2025-07-30 11:03:09 +0900

    Google Analyticsタグを追記

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+// Google tag (gtag.js)
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-R7VS2ZF07K";
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R7VS2ZF07K');
+})();
